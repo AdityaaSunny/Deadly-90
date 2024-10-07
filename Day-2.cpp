4d0094591ff26bf82494fea6a3d6aa511ac95b23
AdityaaSunny <aditya94118@gmail.com>
2024-10-08 00:42:06 +0530

    Day-2

--- Day-2.cpp
+++ Day-2.cpp
@@ -1,6 +1,21 @@
-//https://leetcode.com/problems/divide-two-integers/description/
-               // 29-Divide Two Integers
-
+// https://leetcode.com/problems/divide-two-integers/description/
+//  29-Divide Two Integers
 
 #include <bits/stdc++.h>
 using namespace std;
+int divide(int dividend, int divisor)
+{
+    if (dividend == INT_MIN && divisor == -1)
+        return INT_MAX;
+    long long int ans = dividend / divisor;
+    if (ans > INT_MAX)
+        return INT_MAX;
+    if (ans < INT_MIN)
+        return INT_MIN;
+    return ans;
+}
+int main(){
+    int a = 10;
+    int b = 3;
+    cout<<divide(a,b);
+}
